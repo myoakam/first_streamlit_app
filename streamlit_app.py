7dc3d3e0bd7f618235d300004d0a5229cd2ebedd
myoakam <122029460+myoakam@users.noreply.github.com>
2023-01-09 14:44:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,10 +53,10 @@ def get_fruit_load_list():
  
 
 #Add a button to load the fruit
-#if streamlit.button('Get Fruit Load List'):
-#  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#  my_data_rows = get_fruit_load_list()
-#  streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
   
 #New New Section to display fruityvice api reposonse
