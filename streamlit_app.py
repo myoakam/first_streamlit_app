ff9170100dd40c149f33295adab2bc645efea567
myoakam <122029460+myoakam@users.noreply.github.com>
2023-01-09 11:32:35 -0500

    Update streamlit_app.py
    lesson 12 more changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 #New New Section to display fruityvice api reposonse
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if add_my_fruit != '' : streamlit.write('Thanks for adding ', add_my_fruit)
+  
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
