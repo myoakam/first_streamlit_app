063e71e357d524d3d0e722a0f44f945eb9f78d14
myoakam <122029460+myoakam@users.noreply.github.com>
2023-01-09 15:41:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)   
     
-    
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+  
 #streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 #def get_fruit_load_list():
