2e9082a5e3c655464cc28aa27ee7e81a3becfb6c
myoakam <122029460+myoakam@users.noreply.github.com>
2023-01-09 16:54:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+#new section to distplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -44,12 +45,18 @@ try:
 except URLError as e:
     streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)   
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
+ 
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
 
@@ -58,19 +65,8 @@ streamlit.write('Thanks for Adding', fruit_choice)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")  
   
-#streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
-#def get_fruit_load_list():
-#  with my_cnx.cursor() as my_cur:
-#    my_cur.execute("select * from fruit_load_list")
-#    return my_cur.fetchall()
- 
 
-#Add a button to load the fruit
-#if streamlit.button('Get Fruit Load List'):
-#  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#  my_data_rows = get_fruit_load_list()
-#  streamlit.dataframe(my_data_rows)
+
 
   
 #New New Section to display fruityvice api reposonse
